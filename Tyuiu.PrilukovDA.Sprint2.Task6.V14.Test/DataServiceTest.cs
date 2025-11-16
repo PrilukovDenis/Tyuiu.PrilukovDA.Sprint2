df7185b69d8e9cfaca5e1c21f72b5e3e6c195ba8
deusvela <d.prilukov@aerokod.ru>
2025-11-16 22:45:55 +0500

    Task6

---
+++ Tyuiu.PrilukovDA.Sprint2.Task6.V14.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.PrilukovDA.Sprint2.Task6.V14.Lib;
+
+namespace Tyuiu.PrilukovDA.Sprint2.Task6.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            Assert.AreEqual("Суббота", ds.FindDayName(7, 7));
+        }
+    }
+}
---
+++ Tyuiu.PrilukovDA.Sprint2.Task6.V14/Program.cs
@@ -0,0 +1,12 @@
+﻿using Tyuiu.PrilukovDA.Sprint2.Task6.V14.Lib;
+
+DataService ds  = new DataService();
+
+Console.Write("Введите номер дня года ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите день недели ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+string result = ds.FindDayName(k, d);
+Console.WriteLine(result);
